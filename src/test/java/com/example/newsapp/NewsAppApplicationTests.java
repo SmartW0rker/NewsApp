afabb73cb493c24bd5d58198c8fdf0673e5d4c11
Hristijan <hristijancocevski@gmail.com>
2022-11-27 12:09:15 +0100

    Initial commit

---
+++ src/test/java/com/example/newsapp/NewsAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.newsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
